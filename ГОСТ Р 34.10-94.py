b2a536d3bdcff438bf34acb212fad233fac15906
Антон Гулый <91667419+Obn4zh@users.noreply.github.com>
2023-04-24 19:22:04 +0300

    Update ГОСТ Р 34.10-94.py

--- ГОСТ Р 34.10-94.py
+++ ГОСТ Р 34.10-94.py
@@ -1,44 +1,45 @@
-from random import getrandbits, randint
+from random import randint
 from sympy import isprime
 from Crypto.Util import number
-import hashlib
 import gostcrypto as gost
 
 def gen_keys():
-    print("___ГЕНЕРАЦИЯ КЛЮЧЕЙ___")
+    print("___KEY GENERATION___")
     while True:
         q=randint(2**254,2**256)
         # q=number.getPrime(256)
 
-        r=randint(2**244,2**256)
+        generator=randint(2**244,2**256)
         # r=number.getPrime(256)
-
         while True:
-            p=q*r
+            p=q*generator
             if isprime(p+1):
                 break
-            else: r+=1
+            else: generator+=1
         p+=1
-        if (512>=len(str(bin(p)[2:]))>=509) and (256>=len(str(bin(q)[2:]))>=254) and isprime(p) and isprime(q):
+        if (512>=p.bit_length()>=509) and (256>=q.bit_length()>=254) and isprime(p) and isprime(q):
             break
     print('p:\n',p)
     print('q:\n',q)
 
 
-    print(len(str(bin(p)[2:])))
-    print(len(str(bin(q)[2:])))
+    print(p.bit_length())
+    print(q.bit_length())
     print(isprime(q))
     print(isprime(p))
 
     print((p-1)%q==0)
+#проверка того, что g является корнем порядка q в p
+#это элемент g, который генерирует подгруппу порядка q в мультипликативной группе вычетов по модулю p
 
     while True:
-        g = randint(2, p - 1)
+        g = randint(2, p - 1) 
         a = pow(g, (p-1)//q, p)
         if a != 1:
             break
 
     print('a:\n',a)
+    print(pow(a,q,p)==1)
     x=number.getRandomRange(2, q-1)
     print('x:\n',x)
 
@@ -47,38 +48,38 @@ def gen_keys():
 
     with open("private.pem", "w") as file:
         file.write(str(x))
-        print('Закрытый ключ записан')
+        print('Private key writen')
     with open("public.pem", "w") as file:
         file.write(str(y))
-        print('Открытый ключ записан')
+        print('Public key writen')
     with open("open_a.txt", "w") as file:
         file.write(str(a))
-        print('Открытый параметр a записан')
+        print('Public parameter (a) writen')
     with open("open_p.txt", "w") as file:
         file.write(str(p))
-        print('Открытый параметр p записан')
+        print('Public parameter (p) writen')
     with open("open_q.txt", "w") as file:
         file.write(str(q))
-        print('Открытый параметр q записан')
+        print('Public parameter (q) writen')
 
-    
+
 def read_doc(way):
     with open(way, "rb") as file:
-                # Создаем объект хэш-функции SHA-256 или ГОСТ Р 34.11-94
+                # Создаем объект хэш-функции ГОСТ Р 34.11-94
             hash = gost.gosthash.new('streebog256')
-            # hash = hashlib.sha256()
             while chunk := file.read(256):
                 hash.update(chunk)
                 # Получаем хэш-значение в виде строки
             hash_value = hash.hexdigest()
 
     hash_int=int(hash_value,16)
-    print(f"Хэш:\n{hash_int}\n")
+    print(f"Hash:\n{hash_int}\n")
     # print(len(str(bin(hash_int)[2:])))
     return(hash_int)
 
-def send(hash):
-    print("___ПОДПИСЬ ДОКУМЕНТА___")
+def send(way):
+    hash=read_doc(way)
+    print("___DOCUMENT SIGNING___")
 
     with open("open_q.txt") as file:
         q = file.read()
@@ -97,7 +98,7 @@ def send(hash):
         if r!=0:
             break
     if hash%q==0:
-        hash=1    
+        hash=1
     s=(x*r+k*(hash))%q
 
     with open("r.txt", "w") as file:
@@ -107,10 +108,11 @@ def send(hash):
     print('k:\n',k)
     print('r:\n',r)
     print('s:\n',s)
+    print('\nThe document was signed')
 
 
 def recive(way):
-    print("___ПРОВЕРКА ПОДПИСИ___")
+    print("___SIGNATURE VERIFICATION___")
 
     with open("open_q.txt") as file:
         q = file.read()
@@ -125,7 +127,7 @@ def recive(way):
         r = file.read()
     with open("s.txt") as file:
         s = file.read()
-    
+
     q,p,y,r,s,a=int(q),int(p),int(y),int(r),int(s),int(a)
     hash=read_doc(way)
 
@@ -139,20 +141,23 @@ def recive(way):
     u=(((pow(a,one_z,p))*(pow(y,two_z,p)))%p)%q
     print("u:\n",u)
     if u==r:
-        print("\nПодпись верна!")
+        print("\nThe signature is correct!")
     else:
-        print("\nПодпись неверна!")
-    
+        print("\nThe signature is wrong!")
 
-way="dok.docx"
-hash=read_doc(way)
+
+way="D:\\4 курс\\2 семестр\\КМЗИ\\lab_6(ГОСТ ЭЦП)\\dok.docx"
 
 while True:
-    action=input('\n\nГенерация ключей - 1\nПодписать - 2\nПроверить подпись - 3\nЛюбая другая клавиша - выйти\n')
-    if action=='1':
-        gen_keys()
-    elif action=='2':
-        send(hash)
-    elif action=='3':
-        recive(way)
-    else: quit()
+    try:
+        action=input('\n\nKey generation - 1\nSign a document - 2\nSignature check - 3\nAny other button - quit\n')
+        if action=='1':
+            gen_keys()
+        elif action=='2':
+            send(way)
+        elif action=='3':
+            recive(way)
+    except: print("\nWrong input!")
+    
+    if action!='1' and action!='2' and action!='3': quit()
+    
